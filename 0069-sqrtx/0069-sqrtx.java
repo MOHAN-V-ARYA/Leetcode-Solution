c7cdf9293752835a4baabfa1c6253ccf8f5c0df1
Mohan V <mohanvmohanv178@gmail.com>
2026-07-17 23:01:00 +0530

    Time: 1 ms (97.59%), Space: 42.2 MB (97.84%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int mySqrt(int x) {
+       int sqrt;
+       if(x==0){
+       return 0;
+       }else{
+       sqrt=(int)(Math.sqrt(x));
+       }
+       return sqrt;
+    }
+}
